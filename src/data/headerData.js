3860ebe452012469ab872e9e139886b0a8a685a8
Nasif Kabir <nasifjubian37@gmail.com>
2025-02-17 04:33:25 +0600

    fixed2

--- src/data/headerData.js
+++ src/data/headerData.js
@@ -3,6 +3,6 @@
 export const headerData = {
     name: 'Foyshal Ahmed Biswas',
     title: "Technical Support Engineer",
-    desciption:"Passionate about technology and education, I specialize in troubleshooting, system optimization, and customer support as a Technical Support Engineer at Innovate Solutions. With a background in Computer Science and Engineering, I actively engage in learning and knowledge-sharing, aspiring to inspire the next generation of tech enthusiasts.",
+    desciption:"Passionate about technology and education, I specialize in troubleshooting, system optimization, and customer support as a Technical Support Engineer at Innovate Solutions.",
     image: '/foyshal.jpg',
 }
