541d4145221f26741138a1bb263ba9f17f46620d
Nasif Kabir <nasifjubian37@gmail.com>
2025-02-17 03:18:40 +0600

    edited2

--- src/data/experienceData.js
+++ src/data/experienceData.js
@@ -3,7 +3,14 @@ export const experienceData = [
         id: 1,
         company: 'Innovate Solution',
         jobtitle: 'Technical Support Engineer',
-        startYear: '2024',
+        startYear: 'Dec 2024',
         endYear: 'Running'
+    },
+    {
+        id: 1,
+        company: 'Rainbow Coaching Center, Rajshahi',
+        jobtitle: 'Assistant Teacher',
+        startYear: 'July 2024',
+        endYear: 'Nov 2024'
     },
 ]
